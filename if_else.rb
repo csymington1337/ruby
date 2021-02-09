411d6526e51fb6720a4422764be2cb667fc63600
csymington1337 <symingtonchristina@gmail.com>
2021-02-08 21:51:00 -0700

    Added elsif conditionals

---
+++ if_else.rb
@@ -0,0 +1,16 @@
+fav_color = 'taupe'
+if fav_color == 'red'
+    puts "Red like fire!"
+elsif fav_color == 'orange'
+    puts "Orange like... an orange."
+elsif fav_color == 'yellow'
+    puts "Yellow daffodils are pretty!"
+elsif fav_color == 'green'
+    puts "Have you seen the Wizard in Emerald City?"
+elsif fav_color == 'blue'
+    puts "Blue like the sky!"
+elsif fav_color == 'purple'
+    puts "Only the coolest people like purple. ;)"
+else
+    puts "Wtf colour is THAT?!"
+end
