28026de673842bef43e28e5791649ebe5e0a2b2f
csymington1337 <symingtonchristina@gmail.com>
2021-02-10 21:37:34 -0700

    Added new file with case conditionals

---
+++ case.rb
@@ -0,0 +1,14 @@
+weather = 'colder'
+
+case weather
+when weather == 'sunny'
+    puts "It puts the sunscreen on its skin!"
+when weather == 'cloudy'
+    puts "Wear a sweater in case it gets chilly."
+when weather == 'rainy'
+    puts "Bring an umbrella, ella, ella..."
+when weather == 'cold'
+    puts "ALL THE FLANNEL."
+when weather == 'colder'
+    puts "Just stay inside today. It's not like your car will start anyway!"
+end
