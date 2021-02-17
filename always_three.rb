a02d5a1b5b0213a862d06530bf47a2a20900c254
csymington1337 <symingtonchristina@gmail.com>
2021-02-16 21:45:12 -0700

    Updated always_three file with new script

--- always_three.rb
+++ always_three.rb
@@ -1,9 +1,6 @@
 puts "Give me a number."
-first_number=gets.to_i
-second_number=first_number
-second_number+=5
-second_number*=2
-second_number-=4
-second_number/=2
-second_number-=first_number
-puts "Always #{second_number}"
+$stdout.flush
+
+my_number = gets.to_i
+puts "Always " + (((my_number + 5) * 2 - 4) / 2 - my_number).to_s
+$stdout.flush
