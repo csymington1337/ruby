dd8c558776b9d1ad10a38af9a65bc21a3fa6cd14
csymington1337 <symingtonchristina@gmail.com>
2021-02-18 22:27:04 -0700

    Trying out some methods

--- always_three.rb
+++ always_three.rb
@@ -1,9 +1,10 @@
-def always_three
-    puts "Give me a number."
-    $stdout.flush
-    my_number = gets.to_i
+def always_three(my_number)
     puts "Always " + (((my_number + 5) * 2 - 4) / 2 - my_number).to_s
     $stdout.flush
 end
 
-always_three
+puts "Give me a number."
+$stdout.flush
+my_number = gets.to_i
+
+always_three(my_number)
